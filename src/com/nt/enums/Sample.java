1dae591bb8176158c9be4545bef74d2697b5fb75
rashid1130 <khanrashidkhan012@gmail.com>
2021-03-15 11:12:08 +0530

    Fifth commit

--- src/com/nt/enums/Sample.java
+++ src/com/nt/enums/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 System.out.println("Rashid Khan");
 System.out.println("Mazhar Khan");
-System.out.println("Manjeet Khan");
+
 System.out.println("Chandan");
 	}
 
